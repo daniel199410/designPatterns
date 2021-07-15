4abfa0f7cb6e6a19391fade576ed653bd26d9ddd
dcatanor <dasniel199410@gmail.com>
2021-07-15 16:15:42 -0500

    Command pattern

--- src/behavioral/command/test/CommandTest.java
+++ src/behavioral/command/test/CommandTest.java
@@ -1,8 +1,11 @@
 package behavioral.command.test;
 
+import behavioral.command.FileInvoker;
 import behavioral.command.FileSystemReceiver;
+import behavioral.command.OpenFileCommand;
 import behavioral.command.UnixFileSystemReceiver;
 import behavioral.command.WindowsFileSystemReceiver;
+import behavioral.command.WriteFileCommand;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -11,12 +14,16 @@ public class CommandTest {
     @Test
     public void shouldOpenFileInUnix() {
         FileSystemReceiver fileSystemReceiver = new UnixFileSystemReceiver();
-        assertEquals("Opening file in Unix", fileSystemReceiver.open());
+        OpenFileCommand openFileCommand = new OpenFileCommand(fileSystemReceiver);
+        FileInvoker fileInvoker = new FileInvoker(openFileCommand);
+        assertEquals("Opening file in Unix", fileInvoker.execute());
     }
 
     @Test
     public void shouldWriteFileInWindows() {
         FileSystemReceiver fileSystemReceiver = new WindowsFileSystemReceiver();
-        assertEquals("Writing file in Windows", fileSystemReceiver.write());
+        WriteFileCommand writeFileCommand = new WriteFileCommand(fileSystemReceiver);
+        FileInvoker fileInvoker = new FileInvoker(writeFileCommand);
+        assertEquals("Writing file in Windows", fileInvoker.execute());
     }
 }
