7a9ffa331d6e7e2c38b356403d2595444f0a6726
dcatanor <dasniel199410@gmail.com>
2021-08-12 15:53:42 -0500

    Iterator pattern

--- src/behavioral/iterator/IteratorImpl.java
+++ src/behavioral/iterator/IteratorImpl.java
@@ -6,7 +6,7 @@ import java.util.LinkedList;
 public class IteratorImpl {
 
 	public String iteratorList() {
-		StringBuffer buffer = new StringBuffer();
+		StringBuilder buffer = new StringBuilder();
 		Iterable<Integer> numbers = Arrays.asList(1, 2, 3, 4);
 		for (int i : numbers) {
 			buffer.append(i);
@@ -15,7 +15,7 @@ public class IteratorImpl {
 	}
 
 	public String iteratorLinkedList() {
-		StringBuffer buffer = new StringBuffer();
+		StringBuilder buffer = new StringBuilder();
 		LinkedList<Integer> numbers = new LinkedList<>();
 		numbers.add(1);
 		numbers.add(2);
